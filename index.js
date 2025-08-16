cf098652073bd14799f000acf66da4ba8fc5bcba
rccministeriodecomunicaciones-svg <rccministeriodecomunicaciones@gmail.com>
2025-08-16 01:05:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 5000;
+const port = process.env.PORT || 3000;
 const session = require('express-session');
 const flash = require('connect-flash');
 const dotenv = require('dotenv');
